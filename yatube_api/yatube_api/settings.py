8745104731645c31ea84f2863bbb22045dbad7d5
zhss1983 <zhss1983@gmail.com>
2021-09-28 00:19:21 +0500

    Первое ревью. Поправил settings.py

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -102,6 +102,6 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-   'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
-   'AUTH_HEADER_TYPES': ('Bearer',),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
+    'AUTH_HEADER_TYPES': ('Bearer', ),
 }
